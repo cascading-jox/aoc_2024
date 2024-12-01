30008e456881e134454b4473cdc3cf207292cb22
cascading-jox <cascading.jox@gmail.com>
2024-12-01 11:21:26 +0100

    feat: d1p1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,10 +1,40 @@
 use anyhow::Result;
 use std::fs;
+use std::io::{self, BufRead};
+
+pub fn solve_part_one(left: &[i32], right: &[i32]) -> i32 {
+    let mut left_list = left.to_vec();
+    let mut right_list = right.to_vec();
+
+    left_list.sort();
+    right_list.sort();
+
+    let mut total_distance = 0;
+    for n in 0..left_list.len() {
+        let diff = left_list[n] - right_list[n];
+        total_distance += diff.abs();
+    }
+
+    total_distance
+}
 
 pub fn part_one() -> Result<String> {
-    let input = fs::read_to_string("input/day01.txt")?;
-    // Implement solution for part one
-    Ok("Not implemented yet".to_string())
+    let input = std::fs::File::open("input/day01.txt")?;
+    let reader = io::BufReader::new(input);
+
+    let mut left_list = vec![];
+    let mut right_list = vec![];
+
+    for line in reader.lines() {
+        // handle line reading in case of failure
+        // shorter way of match on line (line is Result)
+        let line = line?;
+        let mut iter = line.split_ascii_whitespace();
+        left_list.push(iter.next().unwrap().parse::<i32>().unwrap());
+        right_list.push(iter.next().unwrap().parse::<i32>().unwrap());
+    }
+
+    Ok(solve_part_one(&left_list, &right_list).to_string())
 }
 
 pub fn part_two() -> Result<String> {
@@ -20,13 +50,15 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one().unwrap();
-        assert_eq!(result, "Not implemented yet");
+        let left = vec![3, 4, 2, 1, 3, 3];
+        let right = vec![4, 3, 5, 3, 9, 3];
+
+        assert_eq!(solve_part_one(&left, &right), 11);
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two().unwrap();
-        assert_eq!(result, "Not implemented yet");
-    }
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two().unwrap();
+    //     assert_eq!(result, "Not implemented yet");
+    // }
 }
